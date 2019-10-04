92a8e77b666f11a4594e23e6df93786b1bd1ca83
Soran <soran.gdr.cs@gmail.com>
2019-10-05 02:25:36 +0330

    alphaBetaFilter function: 1. Defined and initiated variables

--- alphaBetaFilter.py
+++ alphaBetaFilter.py
@@ -8,3 +8,11 @@ def alphaBetaFilter(x, alpha, beta):
     :param beta:
     :return:
     """
+    # x = np.array(x)
+    n = x.__len__()
+    xk_1 = 0
+    vk_1 = 0
+    xk = 0
+    vk = 0
+    rk = 0
+    f = np.zeros((n,), dtype=int)
