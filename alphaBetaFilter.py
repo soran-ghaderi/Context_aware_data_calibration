fb308702d40918ac97ae7bbd047b9ccf395d50c2
Soran <soran.gdr.cs@gmail.com>
2019-10-06 02:32:57 +0330

    alphaBetaFilter function: 1. Removed redundant codes 2

--- alphaBetaFilter.py
+++ alphaBetaFilter.py
@@ -23,9 +23,6 @@ def alphaBetaFilter(
     n = x.__len__()
     xk_1 = 0
     vk_1 = 0
-    xk = 0
-    vk = 0
-    rk = 0
     f = np.zeros((n,), dtype=int)
     for i in range(n):
         xk = xk_1 + vk_1
