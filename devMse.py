c09dd3c2c51d37a19b85dbbd3368f05574f1399e
Soran <soran.gdr.cs@gmail.com>
2019-10-09 02:49:21 +0330

    devMse function: 1. Completed the for loop

--- devMse.py
+++ devMse.py
@@ -11,3 +11,5 @@ def devMse(x, y):
     # y = np.array(y)
     n = min(x.__len__(), y.__len__())
     summation = 0
+    for i in range(n):
+        summation = summation + math.pow((x[i] - y[i]), 2)
