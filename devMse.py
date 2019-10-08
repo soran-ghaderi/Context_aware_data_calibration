d0f93227c37456de5a91e871942f0db230130af4
Soran <soran.gdr.cs@gmail.com>
2019-10-09 02:48:59 +0330

    devMse function: 1. Defined variables 2. Initiated variables

--- devMse.py
+++ devMse.py
@@ -1,7 +1,13 @@
 import math
 
-def devMse():
+def devMse(x, y):
     """
 
+    :param x:
+    :param y:
     :return:
     """
+    # x = np.array(x)
+    # y = np.array(y)
+    n = min(x.__len__(), y.__len__())
+    summation = 0
