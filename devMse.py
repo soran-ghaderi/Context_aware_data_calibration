a0ded75078070a44d35834fa05e5fb8389bf361e
Soran <soran.gdr.cs@gmail.com>
2019-10-12 02:54:46 +0330

    devMse function: 1. Removed redundant codes

--- devMse.py
+++ devMse.py
@@ -9,8 +9,6 @@ def devMse(
     :param y:
     :return: MSE result
     """
-    # x = np.array(x)
-    # y = np.array(y)
     n = min(x.__len__(), y.__len__())
     summation = 0
     for i in range(n):
