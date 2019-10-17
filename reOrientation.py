341bbecf2d685048d172ad46d9f44a89c09aa497
Soran <soran.gdr.cs@gmail.com>
2019-10-17 04:37:00 +0330

    reorientation function: 1. Initiated the function 2. Added docstring

--- reOrientation.py
+++ reOrientation.py
@@ -1,2 +1,12 @@
 import math
 import numpy as np
+
+def reorientation(v, teta_x, teta_y, teta_z):
+    """
+
+    :param v:
+    :param teta_x:
+    :param teta_y:
+    :param teta_z:
+    :return:
+    """
