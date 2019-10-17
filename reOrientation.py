11e5fe345c20b86d7854f1516db5f10bd2e4e877
Soran <soran.gdr.cs@gmail.com>
2019-10-17 04:37:51 +0330

    reorientation function: 1. Calculated R_x

--- reOrientation.py
+++ reOrientation.py
@@ -10,3 +10,6 @@ def reorientation(v, teta_x, teta_y, teta_z):
     :param teta_z:
     :return:
     """
+    R_x = np.array([[1, 0, 0],
+                    [0, math.cos(teta_x), -math.sin(teta_x)],
+                    [0, math.sin(teta_x), math.cos(teta_x)]])
