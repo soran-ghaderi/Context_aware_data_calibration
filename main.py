95c9f7919cecdf1454e0f15a07c53a7546e10084
Soran <soran.gdr.cs@gmail.com>
2019-10-21 04:48:58 +0330

    main.py module 1. Defined variables 2. Initiated variables

--- main.py
+++ main.py
@@ -10,3 +10,11 @@ from devMse import devMse
 from reOrientation import reorientation
 
 input_data = pd.read_excel(r'input.xlsx')
+
+summation = np.zeros((6,), dtype=int)
+mse_sum = np.zeros((6,), dtype=int)
+b_mse_sum = np.zeros((6,), dtype=int)
+dtw_sum = np.zeros((6,), dtype=int)
+b_dtw_sum = np.zeros((6,), dtype=int)
+iter_num = 0
+index = 330
