be3db0939ed86281f240291f5c7167f3b994511e
Soran <soran.gdr.cs@gmail.com>
2019-10-28 05:35:41 +0330

    test_alphaBetaFilter function: 1. Initialised the function 2. Added docstring

--- test/test_alphaBetaFilter.py
+++ test/test_alphaBetaFilter.py
@@ -3,4 +3,8 @@ import numpy as np
 from alpha_beta_filter import alphaBetaFilter
 
 class TestAlphaBetaFilter(TestCase):
-    ""
+    def test_alphaBetaFilter(self):
+        """
+
+        :return:
+        """
