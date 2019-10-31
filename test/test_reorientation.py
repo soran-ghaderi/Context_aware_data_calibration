a41e71676eb8e37f48f125987ab34914e2f70ba8
Soran <soran.gdr.cs@gmail.com>
2019-10-31 05:45:39 +0330

    Refactored the codes

--- test/test_reorientation.py
+++ test/test_reorientation.py
@@ -4,10 +4,10 @@ from unittest import TestCase
 from re_orientation import reorientation
 import numpy as np
 
+
 class TestReorientation(TestCase):
     def test_reorientation(self):
         """
-
         :return: Checks if 'res' is None or not
         """
         teta_x = 2 * math.pi * secrets.SystemRandom().random()
