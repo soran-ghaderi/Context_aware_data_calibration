6b92906448002d7ebb8d9d7183701e343263eca6
Soran <soran.gdr.cs@gmail.com>
2019-10-31 05:46:12 +0330

    Refactored the codes test_devMse.py

--- test/test_devMse.py
+++ test/test_devMse.py
@@ -2,6 +2,7 @@ import unittest
 import numpy as np
 from mse import devMse
 
+
 class TestDevMse(unittest.TestCase):
     def test_devMse(self):
         """
@@ -11,6 +12,7 @@ class TestDevMse(unittest.TestCase):
         y = np.random.rand(6, 1)
         res = devMse(x, y)
         self.assertIsNone(res, 'Successfully Tested.')
+
 
 if __name__ == '__main__':
     unittest.main()
